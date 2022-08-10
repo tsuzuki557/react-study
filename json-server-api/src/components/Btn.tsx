46bd8afa6073d78701c26db222845eb4a38fbcf4
tsuzuki557 <goodin557@gmail.com>
2022-08-10 13:18:10 +0900

    MouseEvent

--- json-server-api/src/components/Btn.tsx
+++ json-server-api/src/components/Btn.tsx
@@ -1,11 +1,17 @@
+import React from "react";
+
 type BtnProps = {
   txt: string;
   children: React.ReactNode;
 };
 
+const clickHander = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  console.log(e);
+};
+
 const Btn: React.FC<BtnProps> = (props) => {
   return (
-    <button>
+    <button onClick={clickHander}>
       ボタン<span>({props.txt})</span>
       {props.children}
     </button>
